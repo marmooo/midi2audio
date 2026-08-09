cee57b88a9d368acafe4d96400c4e3a1e2276534
marmooo <marmooo@users.noreply.github.com>
2026-08-09 21:37:52 +0900

    bump @marmooo/midy from 0.5.8 to 0.6.1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Midy } from "https://cdn.jsdelivr.net/gh/marmooo/midy@0.5.8/dist/midy.min.js";
+import { Midy } from "https://cdn.jsdelivr.net/gh/marmooo/midy@0.6.1/dist/midy.min.js";
 import {
   // AdtsOutputFormat,
   AudioBufferSource,
